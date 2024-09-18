e5906e9e65709d9ab485c9347b617ec2e3179e62
Ron <ron19102004@gmail.com>
2024-09-18 17:21:06 +0700

    fix

--- src/routes/auth.route.php
+++ src/routes/auth.route.php
@@ -1,5 +1,5 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT'] . "/ron/src/utils/import.util.php";
+require "../utils/import.util.php";
 Import::controllers(["auth.controller.php"]);
 class AuthRoute extends Route
 {
